735adb25c6e9d80110565abce9f84e0ab6fd7a29
Calum Couch <ccouch@lcs.on.ca>
2022-11-15 14:47:03 -0500

    Initial Commit

---
+++ PhoneAppInterfaceM2/PhoneAppInterfaceM2App.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneAppInterfaceM2App.swift
+//  PhoneAppInterfaceM2
+//
+//  Created by calum couch on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct PhoneAppInterfaceM2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
